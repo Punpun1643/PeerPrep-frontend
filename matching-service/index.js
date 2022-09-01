8ed0274bf9cc1d8d86f8c010f4665d5a629e3442
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-09-01 17:50:21 +0800

    Initialize socket server instance

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()); // config cors so that front-end can use
@@ -12,6 +16,11 @@ app.get('/', (req, res) => {
     res.send('Hello World from matching-service');
 });
 
-const httpServer = createServer(app);
+// log to console when there is a connection from the client
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
-httpServer.listen(8001);
+httpServer.listen(8001, () => {
+    console.log('listening on port 8001');
+});
