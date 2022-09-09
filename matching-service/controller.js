acde2e1c3ee665061c4f65101476c7dd33374a71
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-09-10 02:59:06 +0800

    Add full CRUD functionality for the database

--- matching-service/controller.js
+++ matching-service/controller.js
@@ -4,8 +4,12 @@ const pendingMatchController = {
     addPendingMatch: addPendingMatch,
     deleteByUsername: deleteByUsername,
     findAllPendingMatches: findAllPendingMatches,
+    findPendingMatchByUsername: findPendingMatchByUsername,
+    updatePendingMatch: updatePendingMatch,
+    updatePendingMatchDifficulty: updatePendingMatchDifficulty
 };
 
+/** Adds pending match to the database. */
 function addPendingMatch(req, res) {
     const pendingMatch = req.body;
     pendingMatchOrm.create(pendingMatch).then(
@@ -17,6 +21,7 @@ function addPendingMatch(req, res) {
     });
 }
 
+/** Deletes pending match from the database by username. */
 function deleteByUsername(req, res) {
     pendingMatchOrm.deleteByUsername(req.params.username).then(
         (data) => {
@@ -30,10 +35,50 @@ function deleteByUsername(req, res) {
     });
 }
 
+/** Retrieves all pending matches in the current database. */
 function findAllPendingMatches(req, res) {
     pendingMatchOrm.findAllPendingMatches().then(
         (data) => {
             res.send(data);
+        },
+    ).catch((err) => {
+        console.log(err);
+    });
+}
+
+/** Finds pending match by username. */
+function findPendingMatchByUsername(req, res) {
+    pendingMatchOrm.findPendingMatchByUsername(req.params.username).then(
+        (data) => {
+            res.send(data);
+        },
+    ).catch((err) => {
+        console.log(err);
+    });
+}
+
+/** Updates an existing pending match by information. */
+function updatePendingMatch(req, res) {
+    pendingMatchOrm.updatePendingMatch(req.body, req.params.username).then(
+        (data) => {
+            res.status(200).json({
+                message: 'Pending match updated successfully!',
+                pendingMatch: data,
+            });
+        },
+    ).catch((err) => {
+        console.log(err);
+    });
+}
+
+/** Updates an existing pending match difficulty level. */
+function updatePendingMatchDifficulty(req, res) {
+    pendingMatchOrm.updatePendingMatchDifficulty(req.body, req.params.username).then(
+        (data) => {
+            res.status(200).json({
+                message: 'Pending match updated successfully!',
+                pendingMatch: data,
+            });
         },
     ).catch((err) => {
         console.log(err);
--- matching-service/pendingMatchRoute.js
+++ matching-service/pendingMatchRoute.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/', pendingMatchController.addPendingMatch);
 router.delete('/:username', pendingMatchController.deleteByUsername);
 router.get('/', pendingMatchController.findAllPendingMatches);
+router.get('/:username', pendingMatchController.findPendingMatchByUsername);
+router.put('/:username', pendingMatchController.updatePendingMatchDifficulty);
 
 export default router;
