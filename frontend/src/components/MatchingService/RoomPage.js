42f3d56f70efab361fd59af48317da899e3a9442
dionegoh <e0439283@u.nus.edu>
2022-09-17 15:47:09 +0800

    Adjust border and margin

--- frontend/src/components/MatchingService/RoomPage.js
+++ frontend/src/components/MatchingService/RoomPage.js
@@ -7,6 +7,7 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from "@mui/material/Typography";
 import LogoutIcon from '@mui/icons-material/Logout';
+import HouseIcon from '@mui/icons-material/House';
 
 
 
@@ -22,29 +23,39 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function RoomPage() {
     return (
         //<Box sx={{backgroundColor: 'blue'}}
-            <Grid container spacing={0} sx={{backgroundColor:'black', width:'100vw', height:'100vh', margin: '0px'}}>
+            <Grid container spacing={0.5} sx={{backgroundColor:'white', width:'100vw', height:'100vh', margin: '0px'}}>
                 {/* left panel */}
-                <Grid item xs={8} md={9}>
-                    <Stack spacing={0}>
+                <Grid item xs={5} md={5}>
+                    <Stack spacing={0.5}>
                         {/* room number and leave room button */}
-                        <Box sx={{height: "10vh", display:'flex', justifyContent:'flex-start', alignItems:'center', backgroundColor: 'white'}}>
-                            <Typography variant="h6" sx={{margin: 2}}> Room 110 </Typography> 
+                        <Box sx={{height: "9.5vh", display:'flex', justifyContent:'flex-start', alignItems:'center', backgroundColor: 'white'}}>
+                            <Typography variant="h6" sx={{margin: 2}}> Room {110} </Typography> 
                             <Button variant="outlined" endIcon={<LogoutIcon />}>
                               Leave Room 
                             </Button>    
                         </Box>
                         {/* question box */}
-                        <Item sx={{height: "50vh"}}>Placeholder question box</Item>
-                        {/*code box */}
-                        <Item sx={{height: "40vh"}}>Placeholder code box</Item>
+                        <Box sx={{height: "58vh", display:'flex', justifyContent:'flex-start', alignItems:'center', 
+                                  backgroundColor: 'white', border: 1.5, borderColor: 'green', borderRadius: 4}}>
+                            <Typography variant="h6" sx={{margin: 2}}> Placeholder question box </Typography> 
+                        </Box>
+                        {/*chat box */}
+                        <Box sx={{height: "30vh", display:'flex', justifyContent:'flex-start', alignItems:'center', 
+                                  backgroundColor: 'white', border: 1.5, borderColor: 'orange', borderRadius: 4}}>
+                            <Typography variant="h6" sx={{margin: 2}}> Placeholder chat box </Typography> 
+                        </Box>
                     </Stack>
                 </Grid>
                 
                 {/* right panel */}
-                <Grid item xs={4} md={3} sx={{height: "100vh"}}>
+                <Grid item xs={6.98} md={6.98} sx={{height: "100vh"}}>
                     <Stack spacing={0}>
-                        {/* chat box */}
-                        <Item sx={{height: "100vh"}}>Placeholder chat box</Item>
+                        {/* code box */}
+                        {/* <Item sx={{height: "100vh"}}>Placeholder code box</Item> */}
+                        <Box sx={{height: "99vh", display:'flex', justifyContent:'flex-start', alignItems:'center', 
+                                  backgroundColor: 'white', border: 1.5, borderColor: 'grey', borderRadius: 4}}>
+                            <Typography variant="h6" sx={{margin: 2}}> Placeholder code box </Typography> 
+                        </Box>
                     </Stack>
                 </Grid>
 
