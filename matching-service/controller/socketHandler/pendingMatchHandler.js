2e8e0d13fb49bf0f0e6f33287e0f3d198a1571ec
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-09-21 15:40:10 +0800

    Send match-success to matched users

--- matching-service/controller/socketHandler/pendingMatchHandler.js
+++ matching-service/controller/socketHandler/pendingMatchHandler.js
@@ -2,9 +2,8 @@ import pendingMatchController from '../pendingMatchController.js';
 
 const pendingMatchHandler = (io) => {
     io.on('connection', (socket) => {
-        console.log('connected to match path');
-
         socket.on('match-easy', async (data) => {
+            // note: maybe need to join from client side?
             socket.join('easy-waiting-room');
             const user = await pendingMatchController.getAvailableMatch('easy');
             // if no match --> add to db
@@ -12,37 +11,30 @@ const pendingMatchHandler = (io) => {
                 pendingMatchController.addPendingMatchEasy(data, socket.id);
             } else {
                 io.to('easy-waiting-room').emit('match-success', socket.id);
-                // if there is a match --> match
+                // else --> match and delete
                 pendingMatchController.deleteMatchByDifficulty('easy');
-                // emit event to a particular socket id
-                // should io emit or socket emit?
-                // socket.emit('match-success', socket.id);
-                // io.to('easy-waiting-room').emit('match-success', socket.id);
             }
         });
 
-        socket.on('match-success', () => {
-            console.log('i have been matched!');
-        });
-
         socket.on('match-medium', async (data) => {
+            socket.join('medium-waiting-room');
             const user = await pendingMatchController.getAvailableMatch('medium');
             if (user === null) {
                 pendingMatchController.addPendingMatchMedium(data, socket.id);
             } else {
+                io.to('medium-waiting-room').emit('match-success', socket.id);
                 pendingMatchController.deleteMatchByDifficulty('medium');
-                io.emit('match-success', socket.id);
             }
         });
 
         socket.on('match-hard', async (data) => {
+            socket.join('hard-waiting-room');
             const user = await pendingMatchController.getAvailableMatch('hard');
             if (user === null) {
                 pendingMatchController.addPendingMatchHard(data, socket.id);
             } else {
                 pendingMatchController.deleteMatchByDifficulty('hard');
-                // io.emit('match-success', socket.id);
-                socket.emit('match-success', socket.id);
+                io.to('hard-waiting-room').emit('match-success-hard', socket.id);
             }
         });
 
