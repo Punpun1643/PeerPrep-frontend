c860a3c86ffa8a62dc9f190f57803dfe8ffe710a
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-09-21 15:14:37 +0800

    Fix match-hard pending match creation bug

--- matching-service/controller/socketHandler/pendingMatchHandler.js
+++ matching-service/controller/socketHandler/pendingMatchHandler.js
@@ -5,15 +5,24 @@ const pendingMatchHandler = (io) => {
         console.log('connected to match path');
 
         socket.on('match-easy', async (data) => {
+            socket.join('easy-waiting-room');
             const user = await pendingMatchController.getAvailableMatch('easy');
             // if no match --> add to db
             if (user === null) {
                 pendingMatchController.addPendingMatchEasy(data, socket.id);
             } else {
+                io.to('easy-waiting-room').emit('match-success', socket.id);
                 // if there is a match --> match
                 pendingMatchController.deleteMatchByDifficulty('easy');
-                io.emit('match-success', socket.id);
+                // emit event to a particular socket id
+                // should io emit or socket emit?
+                // socket.emit('match-success', socket.id);
+                // io.to('easy-waiting-room').emit('match-success', socket.id);
             }
+        });
+
+        socket.on('match-success', () => {
+            console.log('i have been matched!');
         });
 
         socket.on('match-medium', async (data) => {
@@ -29,16 +38,20 @@ const pendingMatchHandler = (io) => {
         socket.on('match-hard', async (data) => {
             const user = await pendingMatchController.getAvailableMatch('hard');
             if (user === null) {
-                pendingMatchController.addPendingMatchMedium(data, socket.id);
+                pendingMatchController.addPendingMatchHard(data, socket.id);
             } else {
                 pendingMatchController.deleteMatchByDifficulty('hard');
-                io.emit('match-success', socket.id);
+                // io.emit('match-success', socket.id);
+                socket.emit('match-success', socket.id);
             }
         });
 
         socket.on('no-match-found', (data) => {
-            pendingMatchController.deletePendingMatchById(data);
+            // pendingMatchController.deletePendingMatchById(data);
+            // send to a particular socket only
         });
+
+        // socket.on('cancel-match', )
     });
 };
 
