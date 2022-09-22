93d2b22364dc63cf28e27ce9e32140d00e01f407
ureshiiYing <tan.ying.hui@u.nus.edu>
2022-09-22 14:47:38 +0800

    Create frontend for change password
    - a button to redirect to change password page
    - change password page with a form for users to fill in their details
    
    - Still need to perform input validation and showing errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
 import {Box} from "@mui/material";
+import ChangePasswordPage from "./components/ChangePasswordPage";
+import ChangePasswordButton from "./components/ChangePasswordButton";
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/chgPassword" element={<ChangePasswordButton/>}/>
+                        <Route path="/changePassword" element={<ChangePasswordPage/>}/>
                     </Routes>
                 </Router>
             </Box>
---
+++ frontend/src/components/ChangePasswordButton
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+
+function ChangePasswordButton() {
+    // redirects to changePasswordPage
+    // current represented as a text link but could be other reps too
+    return (
+        <Box >
+            <Button variant="text" component={Link} to="/ChangePassword">Change Password</Button>
+        </Box>
+    )
+}
+
+export default ChangePasswordButton;
