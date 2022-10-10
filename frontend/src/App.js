535d90cea73f71049d7a449e13861f2c60caba5a
ureshiiYing <tan.ying.hui@u.nus.edu>
2022-10-11 00:55:44 +0800

    Add NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import SignupPage from './components/SignupPage';
 import ChangePasswordPage from "./components/ChangePasswordPage";
 import ChangePasswordButton from "./components/ChangePasswordButton";
@@ -9,6 +9,17 @@ import SelectionView from './components/MatchingService/SelectionView';
 import CountdownView from './components/MatchingService/CountdownView';
 import RoomPage from './components/MatchingService/RoomPage'
 import { useState } from "react";
+import NavBar from "./components/NavBar";
+
+function WithNavBar() {
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+        </>
+    )
+    
+}
 
 
 function App() {
@@ -22,14 +33,17 @@ function App() {
                     <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                     <Route path="/signup" element={<SignupPage/>}/>
                     <Route path="/login" element={<LoginPage />} />
-                    {/* Placeholder home for matching-service */}
-                    <Route path="/home" element={<Home />} />
                     <Route path="/logout" element={<LogoutPage />} />
-                    <Route path="/chgPassword" element={<ChangePasswordButton/>}/>
-                    <Route path="/changePassword" element={<ChangePasswordPage/>}/>
-                    <Route path="/selectquestiondifficulty" element={<SelectionView/>}></Route>
-                    <Route path="/findingmatch" element={<CountdownView/>}></Route>
-                    <Route path="/roompage" element={<RoomPage/>}></Route>
+                    {/* Placeholder home for matching-service */}
+                    <Route path="/" element={<WithNavBar />}>
+                        <Route path="home" element={<Home />} />
+                        <Route path="chgPassword" element={<ChangePasswordButton/>}/>
+                        <Route path="changePassword" element={<ChangePasswordPage/>}/>
+                        <Route path="selectquestiondifficulty" element={<SelectionView/>}></Route>
+                        <Route path="findingmatch" element={<CountdownView/>}></Route>
+                        <Route path="roompage" element={<RoomPage/>}></Route>
+                    </Route>
+                    
                 </Routes>
             </Router>
             {/* </Box> */}
