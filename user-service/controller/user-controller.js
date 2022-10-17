92c404500b7866313b2f5e68b060f9b133e5dd07
fantablack <fantablack@users.noreply.github.com>
2022-10-17 17:32:07 +0800

    Add username cookie

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -143,6 +143,7 @@ export async function loginUser(req, res) {
     // Store token in cookie
     res.cookie('token', token, { expires: new Date(Date.now() + (30 * 60 * 1000)), httpOnly: true });
     res.cookie('refreshToken', refreshToken, { expires: new Date(Date.now() + (30 * 60 * 1000)), httpOnly: true });
+    res.cookie('username', req.body.username, { expires: new Date(Date.now() + (30 * 60 * 1000)) });
 
     return res.status(200).json({
         message: `${user.username} has been authenticated`,
