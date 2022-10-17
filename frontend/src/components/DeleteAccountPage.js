0523f61967954ae12c0210183d7e1c1b507ef7a4
fantablack <fantablack@users.noreply.github.com>
2022-10-17 22:24:52 +0800

    Update DeleteAccountPage.js

--- frontend/src/components/DeleteAccountPage.js
+++ frontend/src/components/DeleteAccountPage.js
@@ -1,8 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from 'react';
 import { Box, Typography } from "@mui/material";
+import axios from "axios";
+import { URL_USER_SVC } from "../configs";
+import { useNavigate } from "react-router-dom";
 
 function DeleteAccountPage(props) {
+    let navigate = useNavigate();
+
+    const ensureLoggedIn = async () => {
+        await axios.post(URL_USER_SVC + '/auth',
+            { withCredentials: true, credentials: 'include' })
+            .catch((err) => {
+                console.log(err)
+                navigate("/login")
+            });
+    }
+
+    useEffect(() => {
+        ensureLoggedIn();
+    })
+
     return (
         <Box display={"flex"} flexDirection={"column"} padding={"4rem"} margin={"auto"}>
             <Typography variant={"h1"}>"Your account has been successfully deleted."</Typography>
