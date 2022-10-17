53e1951b42c699e6afe170209b8c095258356ccd
fantablack <fantablack@users.noreply.github.com>
2022-10-17 22:29:03 +0800

    add protection to selectquestiondifficulty

--- frontend/src/components/MatchingService/SelectionView.js
+++ frontend/src/components/MatchingService/SelectionView.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import DifficultyCard from './DifficultyCard';
 import CountdownView from './CountdownView';
+import axios from "axios";
+import { URL_USER_SVC } from "../../configs";
+import { useNavigate } from "react-router-dom";
 
 
 export default function SelectionView() {
     const difficultyLevels = ["easy", "medium", "hard"];
     const [showModal, setShowModal] = useState(false);
     const [socket, setSocket] = useState(null);
+
+    let navigate = useNavigate();
+
+    const ensureLoggedIn = async () => {
+        await axios.post(URL_USER_SVC + '/auth',
+            { withCredentials: true, credentials: 'include' })
+            .catch((err) => {
+                console.log(err)
+                navigate("/login")
+            });
+    }
+
+    useEffect(() => {
+        ensureLoggedIn();
+    })
 
     const handleOpenModal = (socket) => {
       setShowModal(true);
