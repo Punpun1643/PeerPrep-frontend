a89329d6603f4ca127c2d8902bbd11cb481639f6
ureshiiYing <65227564+ureshiiYing@users.noreply.github.com>
2022-10-30 22:46:20 +0800

    Add prod db
    - separate DB for each microservices using mongodb - user and question

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import UserModel from './user-model.js';
 import 'dotenv/config';
 
-const mongoDB = process.env.ENV === 'PROD' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+const mongoDB = process.env.ENV === 'PROD' ? process.env.DB_CLOUD_URI : process.env.DB_CLOUD_URI_DEV;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
