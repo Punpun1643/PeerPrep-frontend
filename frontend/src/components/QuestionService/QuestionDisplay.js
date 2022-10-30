651deaab7ff5faed1092a83b4ddb0006d306fc48
fantablack <fantablack@users.noreply.github.com>
2022-10-30 22:28:38 +0800

    Implement storing and displaying of pictures for questions (#131)
    **Summary**
    * Questions currently only consist of string data, which may not be sufficient as some questions require image examples to help the user better understand the question.
    * Update Question Bank and Question Schema to support base64 encoded image data.
    * Implement rendering of image data if it exists in `RoomPage.js` and `QuestionDisplay.js`.

--- frontend/src/components/QuestionService/QuestionDisplay.js
+++ frontend/src/components/QuestionService/QuestionDisplay.js
@@ -1,11 +1,17 @@
 import { Box, Typography } from "@mui/material";
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
-function QuestionDisplay({title, body}) {
+function QuestionDisplay({ title, body, image }) {
     return (
-        <Box sx={{whiteSpace: 'pre-line'}}>
-            <Typography variant="h6" sx={{margin: 2}}> {title} </Typography> 
-            <Typography variant="body1" display="block" sx={{margin: 2}}> {body} </Typography> 
-        </Box>
+        <Grid2 sx={{ whiteSpace: 'pre-line' }}>
+            <Typography variant="h6" sx={{ margin: 2 }}> {title} </Typography>
+            {image !== "" &&
+                <Grid2 xs={12}>
+                    <img src={image} alt={title} style={{ width: "100%", height: "100%", objectFit: 'contain' }} />
+                </Grid2>
+            }
+            <Typography variant="body1" display="block" sx={{ margin: 2 }}> {body} </Typography>
+        </Grid2>
     )
 }
 
