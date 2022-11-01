75c0ac136b448079dc151e3c915c219d2f30638d
dionegoh <e0439283@u.nus.edu>
2022-11-02 00:26:46 +0800

    add refresh button on frontend

--- frontend/src/components/QuestionService/QuestionDisplay.js
+++ frontend/src/components/QuestionService/QuestionDisplay.js
@@ -1,10 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
+import AutorenewIcon from '@mui/icons-material/Autorenew';
 
 function QuestionDisplay({title, body}) {
+
+const refreshHandler = () => {
+    console.log('refresh-button clicked');
+}
     return (
         <Box sx={{whiteSpace: 'pre-line'}}>
-            <Typography variant="h6" sx={{margin: 2}}> {title} </Typography> 
-            <Typography variant="body1" display="block" sx={{margin: 2}}> {body} </Typography> 
+            <Box sx={{ display: 'flex',
+                       flexDirection: 'row',
+                       alignItems: 'center', 
+                       padding: 2, 
+                       gap: 1}}>           
+                <Typography variant="h6"> {title} </Typography> 
+                <IconButton onClick={refreshHandler} sx={{backgroundColor: '#F0F0F0'}}>
+                    <AutorenewIcon></AutorenewIcon>
+                </IconButton>
+            </Box>
+                <Typography variant="body1" display="block" sx={{margin: 2}}> {body} </Typography> 
         </Box>
     )
 }
