77b9779bb1e48f9484b99212502d075d756e2a2c
fantablack <fantablack@users.noreply.github.com>
2022-11-05 01:13:26 +0800

    Align grind button and label

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { ensureLoggedIn } from '../Util';
 import CreateIcon from '@mui/icons-material/Create';
 import './Home.css';
+import { flexbox } from '@mui/system';
 
 function Home(props) {
     const [username, setUsername] = useState(Cookies.get('username'));
@@ -17,19 +18,18 @@ function Home(props) {
 
     return (
         <>
-            <div className="homePage">
-                <Box style={{ margin: "auto" }} display={"flex"} flexDirection={"column"} justify-content={"center"}
-                    padding={"4rem"}>
-                    <Typography variant={"h3"} color="white">Welcome back, {username}!</Typography>
+            <Box className="homePage">
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignContent: 'center' }}>
+                    <Typography variant={"h4"} color="white" sx={{ textAlign: 'center', marginTop: 3 }}>Welcome back, {username}!</Typography>
                     <Button
                         href="/selectquestiondifficulty"
                         size="large"
                         variant="contained"
                         color="warning"
                         endIcon={< CreateIcon />}
-                        style={{ "marginTop": 10 }}>Start the Grind </Button>
+                        sx={{ marginTop: 2, alignSelf: 'center' }}>Start the Grind</Button>
                 </Box>
-            </div>
+            </Box>
         </>
     )
 
