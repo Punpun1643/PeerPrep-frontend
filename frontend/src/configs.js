d3f06cb5a7ebb62539bfc2ae74c0dbcb7de1b74c
fantablack <fantablack@users.noreply.github.com>
2022-11-06 00:45:16 +0800

    Update history service port no. and update cors

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,5 +1,5 @@
 const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000'
-const URI_HISTORY_SVC = process.env.URI_HISTORY_SVC || 'http://localhost:8003'
+const URI_HISTORY_SVC = process.env.URI_HISTORY_SVC || 'http://localhost:8004'
 
 const PREFIX_USER_SVC = '/api/user'
 
