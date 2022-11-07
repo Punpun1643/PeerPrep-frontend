d25057a81d447143d0485fe20b75f7c838f94db9
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-11-07 16:17:40 +0800

    Remove backend files

--- matching-service/route/pendingMatchRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import httpMatchController from '../controller/controller.js';
-
-const router = express.Router();
-
-router.get('/', httpMatchController.findAllPendingMatches);
-router.get('/:username', httpMatchController.findPendingMatchByUsername);
-
-export default router;
--- matching-service/routes.js
+++
@@ -1,9 +0,0 @@
-// main route
-import express from 'express';
-import pendingMatchRoutes from './route/pendingMatchRoute.js';
-
-const router = express.Router();
-
-router.use('/pendingMatches', pendingMatchRoutes);
-
-export default router;
